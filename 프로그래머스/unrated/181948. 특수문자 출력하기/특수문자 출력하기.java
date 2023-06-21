7a5fed62d6da94cb1ec2b659e80dadbd1a0a756b
RightAccept <134127144+RightAccept@users.noreply.github.com>
2023-06-22 00:47:30 +0900

    [unrated] Title: 특수문자 출력하기, Time: 113.99 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -1,9 +1,6 @@
 public class Solution {
     public static void main(String[] args) {
-		/*
-		 * 특수문자 출력하기 다음과 같이 출력하도록 코드를 작성해 주세요
-		 * !@#$%^&*(\'"<>?:;
-		 */
+
 		int[] answer = {'!','@','#','$','%','^','&','*','(','\\','\'','\"','<','>','?',':',';'};
 		for(int i = 0; i < answer.length; i++) {
 			System.out.printf("%c",answer[i]);
