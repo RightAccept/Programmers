ac5670525d83389d8e03918d4a1b8b5bc32648d2
RightAccept <134127144+RightAccept@users.noreply.github.com>
2023-06-22 02:07:55 +0900

    [unrated] Title: 더 크게 합치기, Time: 7.31 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int a, int b) {
         int answer = 0;
-		String apb = a + "" + b + "";
-		String bpa = b + "" + a + "";
+		String apb = "" + a + b;
+		String bpa = "" + b + a;
 		
 		answer = apb.compareTo(bpa) >= 0 ? Integer.parseInt(apb) : Integer.parseInt(bpa);
 
