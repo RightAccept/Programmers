a6a5ff37a0336b50b8d2e97e9d27d570b2855d9b
RightAccept <134127144+RightAccept@users.noreply.github.com>
2023-06-23 17:00:59 +0900

    [unrated] Title: 코드 처리하기, Time: 0.01 ms, Memory: 69.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,20 @@
+class Solution {
+	public String solution(String code) {
+		String answer = "";
+		boolean flag = false;
+
+		for (int i = 0; i < code.length(); i++) {
+			if (code.charAt(i) == '1') {
+				flag = !flag;
+			} else if (flag) {
+				answer += i % 2 != 0 ? code.charAt(i) : "";
+			} else {
+				answer += i % 2 == 0 ? code.charAt(i) : "";
+			}
+		}
+		if (answer.equals("")) {
+			answer = "EMPTY";
+		}
+		return answer;
+	}
+}
