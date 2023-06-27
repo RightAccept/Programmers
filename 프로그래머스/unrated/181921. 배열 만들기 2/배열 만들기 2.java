4d69c92cf4e356b608cb110d2cff3c04c22233bb
RightAccept <134127144+RightAccept@users.noreply.github.com>
2023-06-27 15:05:31 +0900

    [unrated] Title: 배열 만들기 2, Time: 5.66 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+class Solution {
+	public int[] solution(int l, int r) {
+		int[] answer = {};
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int count = 0;
+
+		for (int i = l; i <= r; i++) {
+			if (i % 5 == 0) {
+				String tmp = Integer.toString(i);
+				for (int j = 0; j < tmp.length(); j++) {
+					if (tmp.charAt(j) == '5' || tmp.charAt(j) == '0') {
+						count++;
+					} else {
+						count -= 5;
+					}
+				}
+				if (count >= 1) {
+					list.add(i);
+				}
+			}
+			count = 0;
+		}
+		
+		if (list.size() == 0) {
+			list.add(-1);
+		}
+		
+		answer = new int[list.size()];
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = list.get(i);
+		}
+		return answer;
+	}
+}
