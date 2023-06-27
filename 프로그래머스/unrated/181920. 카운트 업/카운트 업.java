4ef74d3f20b8b33b36383b45387a808be5f3c046
RightAccept <134127144+RightAccept@users.noreply.github.com>
2023-06-27 15:11:59 +0900

    [unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -1,16 +1,9 @@
 class Solution {
     public int[] solution(int start, int end) {
-        int[] answer = {};
+        int[] answer = new int[end-start+1];
         int count = 0;
         
-        for(int i = start; i <= end; i++) {
-        	count++;
-        }
-        
-        answer = new int[count];
-        count = 0;
-        
-        for(int i = start; i <= end; i++) {
+        for(int i = start; i<=end; i++) {
         	answer[count++] = i;
         }
         
