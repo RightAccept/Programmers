e4cc3c9ff25587d9d915a9b82d054863ee7ce739
RightAccept <134127144+RightAccept@users.noreply.github.com>
2023-06-27 13:14:12 +0900

    [unrated] Title: 세 개의 구분자, Time: 19.13 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,31 +1,6 @@
 class Solution {
     public String[] solution(String myStr) {
         String[] answer = {};
-		String[] tmp;
-		int index = 0;
-
-		myStr = myStr.replace("a", "_");
-		myStr = myStr.replace("b", "_");
-		myStr = myStr.replace("c", "_");
-		tmp = myStr.split("_");
-
-		for (int i = 0; i < tmp.length; i++) {
-			if (!tmp[i].isEmpty()) {
-				index++;
-			}
-		}
-		answer = new String[index];
-		index = 0;
-
-		for (int i = 0; i < tmp.length; i++) {
-			if (!tmp[i].isEmpty()) {
-				answer[index++] = tmp[i];
-			}
-		}
-		if (answer.length == 0) {
-			answer = new String[1];
-			answer[0] = "EMPTY";
-		}
-		return answer;
-	}
+        return answer;
+    }
 }
