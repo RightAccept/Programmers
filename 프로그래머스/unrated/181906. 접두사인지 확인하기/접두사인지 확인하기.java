66df84e1ac51616604a09995d568062842ebba19
RightAccept <134127144+RightAccept@users.noreply.github.com>
2023-08-03 21:34:51 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,15 +1,8 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
         int answer = 0;
-        String tmp = "";
         
-        for(int i = 0; i < my_string.length(); i++) {
-        	tmp += my_string.charAt(i);
-        	if(is_prefix.equals(tmp)) {
-        		answer = 1;
-        		return answer;
-        	}
-        }
+        if(my_string.startsWith(is_prefix)) answer = 1;
         return answer;
     }
 }
