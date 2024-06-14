ef8fc26a2a3696dedb49d8882ba446f9fbce7663
RightAccept <134127144+RightAccept@users.noreply.github.com>
2024-06-15 01:41:46 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.45 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -9,7 +9,7 @@ class Solution {
             if (dice[0] == dice[3]) {
                 answer = 1111 * dice[3];
             } else if (dice[0] == dice[2] || dice[1] == dice[3]) {
-                answer = (int) Math.pow(dice[1] * 10 + (dice[0] + dice[3] - dice[1]), 2);
+                answer = (int) Math.pow(10 * dice[1] + (dice[0] + dice[3] - dice[1]), 2);
             } else if (dice[0] == dice[1] && dice[2] == dice[3]) {
                 answer = (dice[0] + dice[3]) * (dice[3] - dice[0]);
             } else if (dice[0] == dice[1]) {
