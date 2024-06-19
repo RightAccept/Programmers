5e65ef0f7fca1f66019e3f5158805fc903922f52
RightAccept <134127144+RightAccept@users.noreply.github.com>
2024-06-19 15:52:52 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.04 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        int[] answer = {};
+        int[] tmp1 = Arrays.copyOfRange(arr, intervals[0][0], intervals[0][1] + 1);
+        int[] tmp2 = Arrays.copyOfRange(arr, intervals[1][0], intervals[1][1] + 1);
+        answer = new int[tmp1.length + tmp2.length];
+        System.arraycopy(tmp1, 0, answer, 0, tmp1.length);
+        System.arraycopy(tmp2, 0, answer, tmp1.length, tmp2.length);
+        return answer;
+    }
+}
