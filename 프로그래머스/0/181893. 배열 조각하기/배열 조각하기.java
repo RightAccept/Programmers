250928b8437f56fa3d7e84048c927050a0448acb
RightAccept <134127144+RightAccept@users.noreply.github.com>
2024-07-06 16:50:49 +0900

    [level 0] Title: 배열 조각하기, Time: 0.08 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        int[] answer = {};
+        for (int i = 0; i < query.length; i++) {
+            if (i % 2 == 0) {
+                if (i == 0) {
+                    answer = Arrays.copyOfRange(arr, 0, query[i] + 1);
+                } else {
+                    answer = Arrays.copyOfRange(answer, 0, query[i] + 1);
+                }
+            } else {
+                answer = Arrays.copyOfRange(answer, query[i], answer.length);
+            }
+        }
+        return answer;
+    }
+}
