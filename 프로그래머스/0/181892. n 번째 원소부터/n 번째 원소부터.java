2cedaa875e73b74075e2b312f8eb2374e7be6522
RightAccept <134127144+RightAccept@users.noreply.github.com>
2024-07-07 22:00:04 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,13 +1,9 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
         int[] answer = {};
-        answer = new int[num_list.length - n + 1];
-        int idx = 0;
-        for (int i = 0; i < num_list.length; i++) {
-            if (i + 1 >= n) {
-                answer[idx++] = num_list[i];
-            }
-        }
+        answer = Arrays.copyOfRange(num_list, n - 1, num_list.length);
         return answer;
     }
 }
